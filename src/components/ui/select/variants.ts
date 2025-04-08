ce5b19f81a6d30a48ab11265d0dec87787ed6f93
AdrianKnapp <adrknapp@gmail.com>
2025-04-08 18:02:04 -0300

    feat(ui): improved focus feedback

--- src/components/ui/select/variants.ts
+++ src/components/ui/select/variants.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority"
 
 export const selectTriggerVariants = cva(
-  "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-sm border border-input bg-transparent px-3 py-2 text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
+  "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-sm border border-input bg-transparent px-3 py-2 text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:border-slate-500 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
 )
 
 export const selectContentVariants = cva(
